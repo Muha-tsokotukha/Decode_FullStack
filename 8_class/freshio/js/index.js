0f188c1c5a147efc47e67b14a179796728d748a8
Mukhit <78027029+Muha-tsokotukha@users.noreply.github.com>
2021-11-19 08:52:02 +0600

    freshio

---
+++ 8_class/freshio/js/index.js
@@ -0,0 +1,16 @@
+const aside = document.getElementById("aside").classList.add("active")
+
+function openAside(){
+    document.getElementById("aside").classList.add("active")
+}
+function closeAside(){
+    document.getElementById("aside").classList.remove("active")
+}
+
+function openSecondAside(id){
+    document.getElementById(id).classList.add("active")
+
+}
+function backAside(id){
+
+}
