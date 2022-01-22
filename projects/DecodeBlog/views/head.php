5da0ca794d69fa4a8732c0d3af8ef73de65b0507
Mukhit <78027029+Muha-tsokotukha@users.noreply.github.com>
2022-01-22 19:54:50 +0600

    needs ref

---
+++ projects/DecodeBlog/views/head.php
@@ -0,0 +1,3 @@
+<?php include "config/base_url.php";  ?> 
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
---
+++ projects/DecodeBlog/views/header2.php
@@ -0,0 +1,21 @@
+<div class="nav">
+            
+            <div class="nav-logo">
+                Decode Blog
+            </div>
+
+            <div class="nav-search">
+                <div class="nav-search--bar">
+                    Поиск по блогам
+                </div>
+                <div class="nav-search--search">
+                    <img src="img/search.png" alt="search">
+                    Найти
+                </div>
+            </div>
+
+            <div class="nav-auth">
+                <a href="reg.html">Регистрация</a>
+                <a href="">Вход</a>
+            </div>
+        </div>
